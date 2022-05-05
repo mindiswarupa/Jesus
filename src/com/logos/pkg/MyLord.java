5f507cd52ae31f1846e27fef742fbb6c454d7457
mindiswarupa <mindi.swarupa@gmail.com>
2022-05-05 12:49:33 +0300

    1 commit

---
+++ src/com/logos/pkg/MyLord.java
@@ -0,0 +1,12 @@
+package com.logos.pkg;
+
+public class MyLord {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Lord Jesus");
+						
+	}
+
+}
